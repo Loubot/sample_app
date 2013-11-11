ed8020f257c0704fb9159f599486ba11e7701c28
Louis Angelini <lllouis@yahoo.com>
2013-11-11 05:43:12 +0000

    Morning teabreak

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -12,20 +12,26 @@ describe "Authentication" do
   end
 
   describe "signin" do
-  	before { visit signin_path }
 
-  	describe "with invalid information" do
-  		before { click_button "Sign in" }
+    before { visit signin_path }
 
-  		it { should have_selector('title', text: 'Sign in') }
-  		it { should have_selector('div.alert.alert-error', text: 'Invalid') }
-  	end
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_selector('title', text: 'Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+    end
 
   	describe "with valid information" do
   		let(:user) { FactoryGirl.create(:user) }
   		before do
   			fill_in "Email",				with: user.email.upcase
-  			fill_in "Password"			with: user.password
+  			fill_in "Password",			with: user.password
   			click_button "Sign in"
   		end
 
