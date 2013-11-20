c9515d84ec9fe44792c7f83f03f0f375cb307ecc
Louis Angelini <lllouis@yahoo.com>
2013-11-20 12:04:57 +0000

    Follow and unfollow users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -100,7 +100,17 @@ describe "Authentication" do
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
-        end 
+        end
+
+        describe "visit the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector("title", text: "Sign in") }
+        end
+
+        describe "visit the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector("title", text: "Sign in") }
+        end
       end
 
       describe "in the Microposts controller" do
